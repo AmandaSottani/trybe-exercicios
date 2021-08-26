86c59671e4e6562b0852644b28713c247ac7565b
AmandaSottani <amandasottani@gmail.com>
2021-08-25 22:20:07 -0300

    fiz as quatro primeiras questões dos exercicios do dia 4.1

---
+++ Módulo 1 - fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia1-JavaScript-Primeiros-passos/script.js
@@ -0,0 +1,39 @@
+// questão um
+const a = 5;
+const b = 2;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//questão dois
+const a = 5;
+const b = 15;
+if (a>b){
+    console.log(a)
+}else {
+    console.log(b)
+}
+
+//questão três
+const a = 2;
+const b = 7;
+const c = 10;
+if (a>b && a>c){
+    console.log(a)
+}else if (b>a && b>c){
+    console.log(b)
+}else{
+    console.log(c)
+}
+
+//questão quatro
+let number=0;
+if (number>0){
+    console.log("positive")
+}else if(number<0){
+    console.log("negative")    
+}else{
+    console.log("zero")
+}
