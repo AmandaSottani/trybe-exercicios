079b357066c0475a0287c0f67ec2531491395f83
AmandaSottani <amandasottani@gmail.com>
2021-08-26 21:56:49 -0300

    exercicios 8,9 e inicio do 10

--- Módulo 1 - fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia1-JavaScript-Primeiros-passos/script.js
+++ Módulo 1 - fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia1-JavaScript-Primeiros-passos/script.js
@@ -50,6 +50,63 @@ const anguloTres = -80;
      console.log(false)
  }
 
- //questao seis]
- const nomePeça = "Peão"
- nomePeça.toLowerCase()
+ //questao seis
+ const nomePeca = "BOLA";
+ 
+ if(nomePeca.toLowerCase() == "peão"){
+     console.log("Anda para frente e come em diagonal")
+ }else if (nomePeca.toLowerCase() == "torre"){
+     console.log("Anda em linha reta para frente e para os lados quantas casas quiser")
+ }else if (nomePeca.toLowerCase() == "cavalo"){
+     console.log("Anda em L e pula sobre as peças")
+ }else if (nomePeca.toLowerCase() == "bispo"){
+     console.log("Anda na diagonal e pode andar quantas casas quiser")
+ }else if (nomePeca.toLowerCase() == "rainha"){
+     console.log("Anda para todas as direções quantas casas quiser")
+ }else if (nomePeca.toLowerCase() == "rei"){
+     console.log("Anda em todas as direções apenas uma casa")
+ }else{
+     console.log("Não é um nome de peça válido")
+ }
+
+ //questao sete
+ let porcentagem = -60;
+
+ if(porcentagem>=90 && porcentagem<=100){
+     console.log("A")
+ }else if (porcentagem>=80){
+     console.log("B")
+ }else if(porcentagem>=70){
+     console.log("C")
+ }else if(porcentagem>=60){
+     console.log("D")
+ }else if(porcentagem>=50){
+     console.log("E")
+ }else if(porcentagem<50 && porcentagem>=0){
+     console.log("F")
+ }else{
+     console.log("erro")
+ }
+
+ //questão oito
+ const um = 10;
+ const dois = 12;
+ const tres = 8;
+
+ if((um%2)==0 || (dois%2)==0 || (tres%2)==0 ){
+     console.log(true)
+ }else{
+     console.log(false)
+ }
+
+ //questao nove
+ const um = 10;
+ const dois = 12;
+ const tres = 8;
+
+ if((um%2)!=0 || (dois%2)!=0 || (tres%2)!=0 ){
+     console.log(true)
+ }else{
+     console.log(false)
+ }
+ 
