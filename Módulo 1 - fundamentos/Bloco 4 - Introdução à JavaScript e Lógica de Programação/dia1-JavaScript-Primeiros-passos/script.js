38041d2a2d9e0e69924cc298de213542112e1795
AmandaSottani <amandasottani@gmail.com>
2021-08-27 16:20:22 -0300

    correção do exercicio

--- Módulo 1 - fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia1-JavaScript-Primeiros-passos/script.js
+++ Módulo 1 - fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia1-JavaScript-Primeiros-passos/script.js
@@ -131,7 +131,7 @@ if (salarioBruto<=1556.94){
 }else if(salarioBruto<=2594.92){
     descontoInss = salarioBruto*0.09;
 }else if(salarioBruto<=5189.82){
-    descontoInss = salarioBruto*0,11;
+    descontoInss = salarioBruto*0.11;
 }else{
     descontoInss = 570.88;
 }
